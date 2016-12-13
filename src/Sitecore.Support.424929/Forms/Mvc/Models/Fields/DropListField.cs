3f09db75d61c6806795ba396ceb6c6485782412f
Alexey Lunev <alu@sitecore.net>
2016-12-13 08:41:10 +0200

    Initial patch commit

---
+++ src/Sitecore.Support.424929/Forms/Mvc/Models/Fields/DropListField.cs
@@ -0,0 +1,39 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Forms.Mvc.Models.Fields
+{
+  public class DropListField : Sitecore.Forms.Mvc.Models.Fields.DropListField
+  {
+    // Methods
+    public DropListField(Item item) : base(item)
+    {
+    }
+
+    // Properties
+    public override object Value
+    {
+      get
+      {
+        return base.Value;
+      }
+      set
+      {
+        if (value is List<string>)
+        {
+          if (((List<string>)value).Count > 0)
+          {
+            base.Value = ((List<string>)value)[0];
+          }
+        }
+        else
+        {
+          base.Value = value;
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.424929/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.424929")]
+[assembly: AssemblyProduct("Sitecore.Support.424929")]
+[assembly: ComVisible(false)]
